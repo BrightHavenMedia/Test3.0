3cfacaaa9265611e5ec1575a13f5db5072547cf4
James Jensen <jensen.jedward@gmail.com>
2021-03-31 21:40:48 -0600

    Initial Commit

---
+++ Test3.0/Test3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Test3_0App.swift
+//  Test3.0
+//
+//  Created by James Jensen on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Test3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
